7959105b6c3aae66b3b1d7bbac5cfbe77ce79dd2
Tsuyoshi Akiyama <ka090824@gmail.com>
2020-12-04 17:21:32 +0900

    feat(tools): add CalculateRMSE

--- src/tools.cpp
+++ src/tools.cpp
@@ -4,6 +4,8 @@
 using Eigen::VectorXd;
 using Eigen::MatrixXd;
 using std::vector;
+using std::cout;
+using std::endl;
 
 Tools::Tools() {}
 
@@ -14,6 +16,23 @@ VectorXd Tools::CalculateRMSE(const vector<VectorXd> &estimations,
   /**
    * TODO: Calculate the RMSE here.
    */
+  // Init rmse
+  VectorXd rmse(4);
+  rmse << 0, 0, 0, 0;
+
+  // Check the validity if estimations and ground_truth vector are the same size
+  if (estimations.size() == 0 || estimations.size() != ground_truth.size()) {
+     cout << "Not a valid size, both paramaters should be the same size" << endl;
+     return rmse;
+  }
+
+  for (unsigned int i = 0; i < estimations.size(); i++) {
+     VectorXd res = estimations[i] - ground_truth[i];
+     res = res.array() * res.array();
+     rmse += res;
+  }
+
+  return (rmse / estimations.size()).array().sqrt();
 }
 
 MatrixXd Tools::CalculateJacobian(const VectorXd& x_state) {
