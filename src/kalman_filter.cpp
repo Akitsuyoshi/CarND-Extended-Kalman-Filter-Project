9f4ca1229f6ce1f3033a66994fc7ccc6cb4ed5cf
Tsuyoshi Akiyama <ka090824@gmail.com>
2020-12-06 00:09:15 +0900

    feat(kalman_fitler): modified updateEKF
    Add h function to make cartesian x state to polar and normalization for theta in y

--- src/kalman_filter.cpp
+++ src/kalman_filter.cpp
@@ -1,7 +1,10 @@
 #include "kalman_filter.h"
+#include <iostream>
 
 using Eigen::MatrixXd;
 using Eigen::VectorXd;
+using std::cout;
+using std::endl;
 
 /* 
  * Please note that the Eigen library does not initialize 
@@ -51,8 +54,30 @@ void KalmanFilter::UpdateEKF(const VectorXd &z) {
    * TODO: update the state by using Extended Kalman Filter equations
    */
 
-  VectorXd z_pred = H_ * x_;
-  VectorXd y = z - z_pred;
+  VectorXd hx = VectorXd(3);  // x_ from Cartesian to polar cordinates
+  float px = x_[0], py = x_[1], vx = x_[2], vy = x_[3];
+
+  float position_r = sqrt(px * px + py * py);
+  float theta = atan2(py, px);
+  float velocity_r = (px * vx + py * vy) / position_r;
+
+  hx << position_r,
+        theta,
+        velocity_r;
+
+  VectorXd y = z - hx;
+  
+  // Check if theta in y is between -pi and pi, if not, normalize it to make it within
+  const float pi = 3.141593;
+  while (abs(y[1]) > pi) {
+    // cout << "y[1] = " << y[1] << endl;
+    if (y[1] > pi) {
+      y[1] = y[1] - 2 * pi;
+    } else {
+      y[1] = y[1] + 2 * pi;
+    }
+  }
+  
   MatrixXd PHt = P_ * H_.transpose();
   MatrixXd S = H_ * PHt + R_;
   MatrixXd K = PHt * S.inverse();
